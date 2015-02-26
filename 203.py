9e18c5a123a5c788df130f9de281a2a54cf34b2f
Lorenz Merdian <lorenz.merdian@googlemail.com>
2015-02-26 09:31:49 +0100

    Solution for Challenge 203 (simple)

---
+++ 203.py
@@ -0,0 +1,15 @@
+import turtle
+import time
+import random
+
+t = turtle.Turtle()
+
+t.color('red')
+t.pendown()
+t.goto(100, 0)
+t.goto(100, 100)
+t.goto(0, 100)
+t.goto(0, 0)
+t.hideturtle()
+
+time.sleep(5)
